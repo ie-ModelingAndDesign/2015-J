6f1f1a2ed49ab272a51927c211d8d55babee6c07
onaga <e135770B@ieuser-no-MacBook-Pro-7.local>
2016-02-04 11:06:40 +0900

    add: inputviewを追加

--- new_swift/new_swift/Inputmenu.swift
+++ new_swift/new_swift/Inputmenu.swift
@@ -12,19 +12,23 @@ import RealmSwift
 class Inputmenu: UIViewController, UITextFieldDelegate, UIToolbarDelegate{
     
     private var button: UIButton!
+    private var buttonImage:UIImage = UIImage(named: "image/next_k_12.png")!;
     //　テキストの定義（縦と横の長さ）
-    private let text1: UITextField = UITextField(frame: CGRectMake(0,0,200,30))
-    private var label = UILabel(frame: CGRectMake(0, 50, 200, 20));
-    private var label2 = UILabel(frame: CGRectMake(0, 100, 100, 20));
+    private let text1: UITextField = UITextField(frame: CGRectMake(0,0,120,50))
+    private var label = UILabel(frame: CGRectMake(0, 0, 150, 20));
     
     weak var myTextField: UITextField!
     private var myToolBar: UIToolbar!
     private var myDatePicker: UIDatePicker!
     
-    private var pButton = UIButton(frame: CGRectMake(10, 150, 70, 70));
-    private var pButtonImage:UIImage = UIImage(named: "image/pazdra.png")!;
-    private var mButton = UIButton(frame: CGRectMake(90, 150, 70, 70));
-    private var mButtonImage:UIImage = UIImage(named: "image/monst.png")!;
+    private var Button1 = UIButton(frame: CGRectMake(10, 150, 70, 70));
+    private var Button1Image:UIImage = UIImage(named: "image/pazdra.png")!;
+    private var Button2 = UIButton(frame: CGRectMake(90, 150, 70, 70));
+    private var Button2Image:UIImage = UIImage(named: "image/monst.png")!;
+    private var Button3 = UIButton(frame: CGRectMake(170, 150, 70, 70));
+    private var Button3Image:UIImage = UIImage(named: "image/tumutumu.jpg")!;
+    private var Button4 = UIButton(frame: CGRectMake(250, 150, 70, 70));
+    private var Button4Image:UIImage = UIImage(named: "image/sironeko.png")!;
     private let game: UITextField = UITextField(frame: CGRectMake(0, 0, 200, 30));
     
     
@@ -33,7 +37,7 @@ class Inputmenu: UIViewController, UITextFieldDelegate, UIToolbarDelegate{
         super.init(nibName: nil, bundle: nil)
         
         // tabBarItemのアイコンを設定する
-        self.tabBarItem = UITabBarItem(title: "帳簿", image: UIImage(named: "image/chobo.png"), selectedImage: UIImage(named: "image/chobo.png"))
+        self.tabBarItem = UITabBarItem(title: "帳簿", image: UIImage(named: "image/chobo.png"), selectedImage: UIImage(named: "chobo.png"))
     }
     
     required init?(coder aDecoder: NSCoder){
@@ -48,15 +52,17 @@ class Inputmenu: UIViewController, UITextFieldDelegate, UIToolbarDelegate{
         super.viewDidLoad()
         
         
+        let BackGround = UIView()
+        BackGround.backgroundColor = UIColor(red: 0.6667, green: 0.7451, blue: 0.6667, alpha: 1.0)
+        
         //　表示する文字の設定
         label.text = "課金の帳簿";
         label.font = UIFont.systemFontOfSize(30)
-        label2.text = "日付";
-        label2.font = UIFont.systemFontOfSize(20)
+        label.layer.position = CGPoint(x: self.view.bounds.width/2, y: 50)
         
         
         //　入力前に薄い文字を表示する
-        text1.placeholder = "金額を入力してね"
+        text1.placeholder = "金額を入力"
         
         // 枠を表示する
         text1.borderStyle = UITextBorderStyle.RoundedRect
@@ -76,11 +82,9 @@ class Inputmenu: UIViewController, UITextFieldDelegate, UIToolbarDelegate{
         
         // ボタン配置
         let button = UIButton()
-        button.setTitle("次へ", forState: .Normal)
-        button.frame = CGRectMake(100, 100, 100, 30)
-        button.backgroundColor = UIColor.blueColor()
+        button.frame = CGRectMake(100, 100, 100, 40)
+        button.setBackgroundImage(buttonImage, forState: UIControlState.Normal)
         button.layer.masksToBounds = true
-        button.layer.cornerRadius = 10.0
         button.layer.position = CGPoint(x: self.view.frame.width/2, y: 400)
         button.addTarget(self, action: "tapped", forControlEvents: .TouchUpInside)
         
@@ -88,9 +92,9 @@ class Inputmenu: UIViewController, UITextFieldDelegate, UIToolbarDelegate{
         myTextField = UITextField()
         myTextField.placeholder = dateToString(NSDate())
         myTextField.text = dateToString(NSDate())
-        myTextField.layer.borderColor = UIColor.blueColor().CGColor
+        myTextField.layer.borderColor = UIColor(red: 0.2745, green: 0.3137, blue: 0.2745, alpha: 1.0).CGColor
         myTextField.layer.borderWidth = 2.0
-        myTextField.frame = CGRectMake(100, 100, 130, 30) //(x座標,　y座標, width, height)
+        myTextField.frame = CGRectMake(0, 0, 150, 50) //(x座標,　y座標, width, height)
         myTextField.layer.position = CGPoint(x: self.view.frame.width/2, y: 110)
         self.view.addSubview(myTextField)
         
@@ -115,32 +119,87 @@ class Inputmenu: UIViewController, UITextFieldDelegate, UIToolbarDelegate{
         myTextField.inputAccessoryView = myToolBar
         
         // ゲーム選択ボタン用
-        pButton.setBackgroundImage(pButtonImage, forState: UIControlState.Normal)
-        pButton.addTarget(self, action: "pBtn_click", forControlEvents: .TouchUpInside);
+        Button1.setBackgroundImage(Button1Image, forState: UIControlState.Normal)
+        Button1.addTarget(self, action: "pBtn_click", forControlEvents: .TouchUpInside);
+        Button1.layer.position = CGPoint(x: self.view.frame.width / 5, y: 200)
         
-        mButton.setBackgroundImage(mButtonImage, forState: UIControlState.Normal)
-        mButton.addTarget(self, action: "mBtn_click", forControlEvents: .TouchUpInside);
+        Button2.setBackgroundImage(Button2Image, forState: UIControlState.Normal)
+        Button2.addTarget(self, action: "mBtn_click", forControlEvents: .TouchUpInside);
+        Button2.layer.position = CGPoint(x: self.view.frame.width * 2 / 5, y: 200)
+        
+        Button3.setBackgroundImage(Button3Image, forState: UIControlState.Normal)
+        Button3.addTarget(self, action: "tBtn_click", forControlEvents: .TouchUpInside);
+        Button3.layer.position = CGPoint(x: self.view.frame.width * 3 / 5, y: 200)
+        
+        Button4.setBackgroundImage(Button4Image, forState: UIControlState.Normal)
+        Button4.addTarget(self, action: "sBtn_click", forControlEvents: .TouchUpInside);
+        Button4.layer.position = CGPoint(x: self.view.frame.width * 4 / 5, y: 200)
         
         
         //　viewに追加
         self.view.addSubview(text1)
         self.view.addSubview(label)
-        self.view.addSubview(label2)
         
         self.view.addSubview(button)
-        self.view.addSubview(pButton)
-        self.view.addSubview(mButton)
+        self.view.addSubview(Button1)
+        self.view.addSubview(Button2)
+        self.view.addSubview(Button3)
+        self.view.addSubview(Button4)
+        
+        self.view.backgroundColor = BackGround.backgroundColor
     }
     
     
-    // pButtonが選択されたとき
+    // Button1が選択されたとき
     func pBtn_click(){
+        Button1.alpha = 1.0;
         game.text = "パズドラ"
+        Button2.alpha = 0.1;
+        Button3.alpha = 0.1;
+        Button4.alpha = 0.1;
+        self.view.addSubview(Button1)
+        self.view.addSubview(Button2)
+        self.view.addSubview(Button3)
+        self.view.addSubview(Button4)
     }
     
     // mButtonが選択されたとき
     func mBtn_click(){
+        Button2.alpha = 1.0;
         game.text = "モンスト"
+        Button1.alpha = 0.1;
+        Button3.alpha = 0.1;
+        Button4.alpha = 0.1;
+        self.view.addSubview(Button1)
+        self.view.addSubview(Button2)
+        self.view.addSubview(Button3)
+        self.view.addSubview(Button4)
+    }
+    
+    // tButtonが選択されたとき
+    func tBtn_click(){
+        Button3.alpha = 1.0;
+        game.text = "ツムツム"
+        Button1.alpha = 0.1;
+        Button2.alpha = 0.1;
+        Button4.alpha = 0.1;
+        self.view.addSubview(Button1)
+        self.view.addSubview(Button2)
+        self.view.addSubview(Button3)
+        self.view.addSubview(Button4)
+    }
+    
+    // sButtonが選択されたとき
+    func sBtn_click(){
+        Button4.alpha = 1.0;
+        game.text = "白猫"
+        Button1.alpha = 0.1;
+        Button2.alpha = 0.1;
+        Button3.alpha = 0.1;
+        self.view.addSubview(Button1)
+        self.view.addSubview(Button2)
+        self.view.addSubview(Button3)
+        self.view.addSubview(Button4)
     }
     
     
@@ -165,14 +224,54 @@ class Inputmenu: UIViewController, UITextFieldDelegate, UIToolbarDelegate{
         inputDB.day = day!
         inputDB.game = gamename!
         
-        print("ボタンが押されたよ！")
+        // textFieldの値を取得
+        let inputText = text1.text
+        let inputdate = myTextField.text
+        let inputgame = game.text
+        let myAlert: UIAlertController = UIAlertController(title: "入力されてないよ", message: "ゲーム or 価格の情報がありません", preferredStyle: .Alert)
+        let myAlertOk: UIAlertController = UIAlertController(title: "入力できました!", message: "帳簿に記録します", preferredStyle: .Alert)
+        
+        // ゲームが選択されていない時か、金額が入力されていない時にアラートを表示
+        if ((inputText == "") || (inputgame == "")){
+            let myNgAction = UIAlertAction(title: "OK", style: .Default) { action in
+                print("Action OK!!")
+            }
+            
+            myAlert.addAction(myNgAction)
+            
+            // UIAlertを発動する
+            presentViewController(myAlert, animated: true, completion: nil)
+            
+            // 金額が入力されないと金額入力欄に文字を薄く表示
+            if (inputText == ""){
+                self.view.addSubview(text1)
+            }
+        }
+            
+            // 正しく入力されている時にアラート表示（各要素のデータを表示する）
+        else {
+            let myOkAction = UIAlertAction(title: "OK", style: .Default) { action in
+                print("Action OK!!")
+            }
+            
+            myAlertOk.addAction(myOkAction)
+            
+            // UIAlertを発動する
+            presentViewController(myAlertOk, animated: true, completion: nil)
+            
+            // 入力された値を表示する
+            print(inputText)
+            print(inputdate)
+            print(inputgame)
+        }
+        
         // キーボードを閉じる
         view.endEditing(true)
         
         let config = Realm.Configuration(
-              schemaVersion: 1,
-              migrationBlock: { migration, oldSchemaVersion in
-                    if (oldSchemaVersion < 1) {
+            schemaVersion: 1,
+            migrationBlock: { migration, oldSchemaVersion in
+                if (oldSchemaVersion < 1) {
                 }
         })
         Realm.Configuration.defaultConfiguration = config
